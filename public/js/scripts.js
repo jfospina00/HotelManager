c29f254a8b6b666da9f93cd20f364e6ebfae0e34
jfospina00 <jfospina00@misena.edu.co>
2016-01-21 22:57:17 -0500

    script comentado

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,11 @@
-// $(function() {
-// 	$('.tb-show').click(function(event) {
-// 		/* Act on the event */
-// 		$id    = $(this).attr('id');
-// 		$token = $('#_token1').val();
-// 		$route = $("input[route]").val();
-// 		$.post($route, {id: $id,_token:$token}, function(datos) {
-// 		  	$('.modal-body').html(datos);
-// 		});
-// 	});
-// });
+$(function() {
+	$('.tb-show').click(function(event) {
+		/* Act on the event */
+		$id    = $(this).attr('id');
+		$token = $('#_token1').val();
+		$route = $("input[route]").val();
+		$.post($route, {id: $id,_token:$token}, function(datos) {
+		  	$('.modal-body').html(datos);
+		});
+	});
+});
